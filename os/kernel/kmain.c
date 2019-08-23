0d6aa85a4f6e0f864557adc403b65e81708d792f
Tom Hutchings <goerofmuns@gmail.com>
2019-08-23 11:30:26 +0100

    move mem queriees to kmem and modify makefile to accomodate

--- os/kernel/kmain.c
+++ os/kernel/kmain.c
@@ -2,59 +2,18 @@
 #include <stdint.h>
 
 #include "kterm.h"
-
-size_t k_print_mem_info(size_t addr)
-{
-    //Detect low memory
-    size_t low_mem_size;
-    asm(
-	"clc;"
-	"int 0x12;"
-	"mov %%eax, %0;"
-	: "=r"(low_mem_size));
-
-    k_print();
-
-    //Detect high memory
-    size_t mem_size;
-    
-    asm(
-	"push %%eax;"
-	"mov $0xe820, %%eax;"
-	"int $0x15;"
-	""
-	: "=r"(mem_size));
-
-    return mem_size;
-}
-
-void k_disable_cursor()
-{
-     asm(
-	"pushf     \n"
-	"push %eax \n"
-	"push %edx \n"
-
-	"mov $0x3D4, %dx \n"
-	"mov $0xA, %al   \n"
-	"out %al, %dx    \n"
-
-	"inc %dx         \n"
-	"mov $0x20, %al  \n"
-	"out %al, %dx    \n"
-
-	"pop %edx \n"
-	"pop %eax \n"
-	"popf     \n"
-	"ret      \n"
-	);   
-}
+#include "kmem.h"
 
 // kernel entry point
 void k_main()
 {
     k_term_init();
     k_print("kernel init\n");
-    k_print("sugma");
-    // k_print_bios_info();
+    k_print("lower memory size: ");
+    size_t low_mem = k_query_low_mem();
+    k_print_num(low_mem);
+
+    //switch to protected mode
+    //start scheduler
+    //start shell
 }
--- os/kernel/kmem.c
+++ os/kernel/kmem.c
@@ -1,4 +1,45 @@
-void k_query_mem_map()
+#include <stddef.h>
+#include <stdint.h>
+
+#include "kmem.h"
+
+typedef struct MMapEntry {
+    uint32_t BaseL;
+    uint32_t BaseH;
+    uint32_t LengthL;
+    uint32_t LengthH;
+    uint32_t Type;
+}__attribute__((packed)) MMapEntry_t;
+
+size_t k_query_low_mem()
 {
+    //Detect low memory
+    size_t low_mem_size;
+    asm(
+	".code16gcc;"
+	"push %%eax;"
+	"clc;"
+	"int $0x12;"
+	"mov %%eax, %0;"
+	"pop %%eax"
+	: "=r"(low_mem_size));
+
+    return low_mem_size;
+}
+
+size_t k_query_mem_map(size_t seg, size_t offset)
+{
+    //TODO: first try check using multiboot memmap (grub)
+
+    //setup regs
+    // ax  = 0xE820
+    // ecx = sizeof MMapEntry_t
+    // edx = 'SMAP'
+    // ES:DI = seg:offset
     
+    //call int
+    
+    //loop for entries (ebx != 0)
+    
+    return 0;
 }
