8d1b5fe6557abcb2a6fca4e4379f240dbabf3083
Tom Hutchings <goerofmuns@gmail.com>
2019-11-19 23:40:57 +0000

    gitignore for grub stuff and the make_image script

--- os/make_image.sh
+++ os/make_image.sh
@@ -4,4 +4,4 @@ cp os.bin isodir/boot/
 ../grub/build/grub-mkrescue -o os.iso isodir
 
 #mkdir tmp
-#../grub/build/grub-mkimage -p /boot -o tmp/os.img bultibot sh fat
+#../grub/build/grub-mkimage -p /boot -o tmp/os.img multiboot sh fat
