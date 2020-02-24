4a2abaa918b3d973dfb55a669694061ecf400fd3
Tom Hutchings <goerofmuns@gmail.com>
2020-02-24 20:30:59 +0000

    ini keyboard handler stuff

--- os/kernel/keyboard.c
+++ os/kernel/keyboard.c
@@ -1,6 +1,20 @@
 #include "keyboard.h"
 
+#include "kterm.h"
+
 uint8_t read_scan_code()
 {
     return inb(KBD_PORT);
+}
+
+static void keyboard_handler()
+{
+    k_print("KEY HIT");
+    asm("hlt");
+}
+
+void k_keyboard_init()
+{
+    k_register_interrupt_handler(33, keyboard_handler);
+    k_println("Keyboard driver initialized");
 }
--- os/kernel/keyboard.h
+++ os/kernel/keyboard.h
@@ -5,3 +5,5 @@
 #define KBD_PORT 0x60
 
 uint8_t read_scan_code();
+void k_keyboard_init();
+
